cf0dba243d56f9e2d59342db21640b58a6560e88
Julien REIGNIER <julienreignierr@gmail.com>
2025-01-06 11:23:53 +0100

    wip

--- android/src/main/java/com/swipelive/capacitor/stripe/Stripe.java
+++ android/src/main/java/com/swipelive/capacitor/stripe/Stripe.java
@@ -11,44 +11,30 @@ import com.stripe.android.paymentsheet.PaymentSheetResult;
 
 public class Stripe {
   private PaymentSheet paymentSheet;
-  private PaymentSheetResultCallback resultCallback;
-
   private ActivityResultLauncher<Intent> paymentLauncher;
 
   public void initialize(AppCompatActivity activity, String publishableKey, PaymentSheetResultCallback callback) {
     PaymentConfiguration.init(activity, publishableKey);
-    resultCallback = callback;
 
     paymentLauncher = activity.registerForActivityResult(
-        new ActivityResultContracts.StartActivityForResult(),
+        new PaymentSheet.Contract(),
         result -> {
-          if (paymentSheet != null) {
-            paymentSheet.onPaymentSheetResult(result.getData());
-          } else {
-            resultCallback.onError("PaymentSheet not initialized");
+          if (result instanceof PaymentSheetResult.Completed) {
+            callback.onSuccess();
+          } else if (result instanceof PaymentSheetResult.Canceled) {
+            callback.onCancel();
+          } else if (result instanceof PaymentSheetResult.Failed) {
+            callback.onError(((PaymentSheetResult.Failed) result).getError().toString());
           }
         }
     );
 
-    paymentSheet = new PaymentSheet(activity, this::onPaymentSheetResult);
+    paymentSheet = new PaymentSheet(activity, paymentLauncher::launch);
   }
 
   public void presentPaymentSheet(String clientSecret, PaymentSheet.Configuration configuration) {
-    if (paymentLauncher == null) {
-      resultCallback.onError("PaymentLauncher not initialized");
-      return;
-    }
-
-    paymentSheet.presentWithPaymentIntent(clientSecret, configuration);
-  }
-
-  private void onPaymentSheetResult(PaymentSheetResult result) {
-    if (result instanceof PaymentSheetResult.Completed) {
-      resultCallback.onSuccess();
-    } else if (result instanceof PaymentSheetResult.Canceled) {
-      resultCallback.onCancel();
-    } else if (result instanceof PaymentSheetResult.Failed) {
-      resultCallback.onError(((PaymentSheetResult.Failed) result).getError().toString());
+    if (paymentSheet != null) {
+      paymentSheet.presentWithPaymentIntent(clientSecret, configuration);
     }
   }
 
